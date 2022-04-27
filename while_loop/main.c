8b56e43f59dbb7e32f3d8e374b0c4a4f2c735451
James Leo <48637026+jamesleocodes@users.noreply.github.com>
2022-04-27 16:47:46 +0800

    c

--- while_loop/main.c
+++ while_loop/main.c
@@ -1,17 +1,21 @@
-//
-//  main.c
-//  myProgramme
-//
-//  Created by Zaw on 13/4/2022.
-//
-
+/* This program increases a counter from 1 to 5
+    printing updates and then counts it back down to 1. However,
+    it uses while loops and the increment and decrement operators */
 #include <stdio.h>
 
-int main() {
-    // insert code here...
-    char str_name[100];
-    printf("Enter Your Name : ");
-    scanf("%s, str_name");
-    printf("Hello, %s\n,str_name");
+int main()
+{
+    int ctr = 0;
+    
+    while (ctr < 5)
+    {
+        printf("Counter is at %d.\n",  ++ctr);
+    }
+    
+    while (ctr > 1)
+    {
+        printf("Counter is at %d.\n", --ctr);
+    }
+    
     return 0;
 }
