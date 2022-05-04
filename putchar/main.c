3ebdd02cb4e7ead8e892c8896972daca56793b44
James Leo <48637026+jamesleocodes@users.noreply.github.com>
2022-05-04 10:24:25 +0800

    c

---
+++ putchar/main.c
@@ -0,0 +1,17 @@
+/* This program is nothing more than a simple demonstration of the putchar() function*/
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int i;
+    char msg[] = "C is fun";
+    for (i = 0; i < strlen(msg); i++)
+    {
+        putchar( msg[i]); // Outputs a single character
+    }
+    putchar('\n'); // One line break after the loop is done.
+    
+    return(0);
+}
