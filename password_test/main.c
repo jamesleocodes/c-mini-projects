579d5d9072db1b6f8afd4964c86fc26a16a90237
James Leo <48637026+jamesleocodes@users.noreply.github.com>
2022-05-04 16:40:45 +0800

    c

---
+++ password_test/main.c
@@ -0,0 +1,63 @@
+/* This program asks a user for a username and a password. It test
+    whether their passwrod has an uppercase letter, lowercase letter,
+    and a digit. If it does, the program congratulates their selection.
+    If not, it suggests they might want to consider a password with more variety for the sake of  security. */
+// stdio.h is needed for printf() and scanf()
+// string.h is needed for strlen()
+// ctype.h is needed for isdigit, isupper, and islower
+
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main()
+{
+    int i;
+    int hasUpper, hasLower, hasDigit;
+    char user[25], password[25];
+    
+    // Initialize all three test variables to 0 i.e false
+    
+    hasUpper = hasLower = hasDigit  = 0;
+    printf("What is your username? ");
+    scanf(" %s",user);
+    
+    printf("Please create a password: ");
+    scanf(" %s", password);
+    
+    // This loop goes through each character of the password and
+    // test
+    // whether it is a digit, uppercase letter, then lowercase
+    // letter.
+    for (i = 0; i < strlen(password); i++)
+    {
+        if (isdigit(password[i]))
+        {
+            hasDigit = 1;
+            continue;
+        }
+        if (isupper(password[i]))
+        {
+            hasUpper = 1;
+            continue;
+        }
+       
+        if (islower(password[i]))
+        {
+            hasLower = 1;
+        }
+    }
+    /* The if protion will only excute if all three variables below are 1, and the variables will only equal 1 if the appropriate characters were each found */
+    if ((hasDigit) && (hasUpper) && (hasLower))
+    {
+        printf("\n\nExcellent work, %s,\n", user);
+        printf("Your password has upper and lowercase ");
+        printf("letters and a number. ");
+    } else
+    {
+        printf("\n\nYou should consider a new password, %s,\n",user);
+        printf("One that uses upper and lowercae letters ");
+        printf("and a number. ");
+    }
+    return(0);
+}
