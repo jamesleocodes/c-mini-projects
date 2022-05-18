a080951f9adf1540592578364ec2dc799efa2e15
James Leo <48637026+jamesleocodes@users.noreply.github.com>
2022-05-18 11:36:47 +0800

    c

---
+++ pointer/main.c
@@ -0,0 +1,71 @@
+/* This program demonstrates pointers by declaring and initializing both
+ regular and pointer variable for int float, and char types and then displays the vlaues of each. */
+
+# include <stdio.h>
+
+int main()
+{
+    int kids;
+    int * pKids;  // pointer variable declaration
+    float price;
+    float * pPrice;  // pointer variable declaration
+    char code;
+    char * pCode;  // pointer variable declaration
+    
+    price = 17.50;
+    pPrice = &price; // Store address of price in pointer variable
+    
+    printf("\nHow many kids are you taking to the water park? ");
+    scanf(" %d", &kids);
+    
+    pKids = &kids; // Store address of kids in pointer variable
+    
+    printf("\nDo you have a discount ticket for the park?");
+    printf("\nEnter E for Employee Discount, S for Sav-More ");
+    printf("\nDiscount, or N for No Discount: ");
+    scanf(" %c", &code);
+    
+    pCode = &code;
+    
+    printf("\nFirst let's do it with variable :\n");
+    printf("You've got %d kids..\n", kids);
+    switch (code) {
+        case ('E'):
+            printf("The employee discount aves you 25%% on the ");
+            printf("$%.2f price", price);
+            printf("\nTotal ticket cost; $%.2f", (price*.75*kids));
+            break;
+        case ('S'):
+            printf("The Sav-more discount saves you 15%% on the ");
+            printf("$%.2f price", price);
+            printf("\nTotal ticket cost; $%.2f", (price*.85*kids));
+            break;
+        default : // Either entered N for No discount or
+                  // an invalid letter
+            printf("You will be paying full price of ");
+            printf("$%.2f for your tickets", price);
+    }
+    
+    // Now repeat the same but use dereferenced
+    // pointers and get the same results
+    
+    printf("\n\n\nNow let's do it with the pointers:\n");
+    printf("You've got %d kids...\n",*pKids);
+    switch (*pCode) {
+        case ('E'):
+            printf("The employee discount saves you 25%% on the ");
+            printf("$%.2f price", *pPrice);
+            printf("\nTotal ticket cost: $%.2f", (*pPrice*.75* *pKids));
+            break;
+        case ('S'):
+            printf("The Sav-more discount saves you 15%% on the ");
+            printf("$%.2f price", *pPrice);
+            printf("\nTotal ticket cost $%.2f", (*pPrice * .85 * *pKids));
+            break;
+        default: // Either entered N for No Discount or
+                // an invalid letter
+            printf("You will be paying full price of ");
+            printf("$%.2f for your tickets", *pPrice);
+    }
+    return 0;
+}
